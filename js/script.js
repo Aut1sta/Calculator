cbff1d9b56948617c31beed41d628cdc67d8b3f5
Aut1sta <godoyguilherme799@gmail.com>
2021-09-07 18:11:31 -0300

    Creating the Calculator HTML and JS

---
+++ js/script.js
@@ -0,0 +1,45 @@
+
+// Calculation
+
+function add(a,b){
+    return a + b
+}
+
+function subtract(a,b){
+    return a - b
+}
+
+function multiply(a,b){
+    return a * b
+}
+
+function divide(a,b){
+    return a / b
+}
+
+// Operate
+
+function operate(operator, a, b){
+    a = Number(a)
+    b = Number(b)
+    switch(operator){
+        case '+':
+            return add(a,b)
+            break;
+        case '-':
+            return subtract(a,b)
+            break;
+        case '*':
+            return multiply(a,b)
+            break;
+        case '/':
+            if(b == 0){
+                return null
+            }else{
+                return divide(a,b)
+            }
+            break;
+        default:
+            return null
+    }
+}
